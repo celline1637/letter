78c168ca61f2152e5016e4fa3996c1988431c030
sol <sol@spoonradio.co>
2022-11-23 16:57:04 +0900

    💄 UI: theme 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,27 @@
 import Layout from '@components/layout/Layout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import GlobalStyle from '@theme/globalStyle';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../theme/index';
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
